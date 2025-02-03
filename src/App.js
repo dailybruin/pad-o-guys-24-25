75da0a4538d155484383030a495f03d81bc4dd83
Armaan Bassi <armalbassi@gmail.com>
2025-02-03 00:43:42 -0800

    fixed hover effect + how images are handled

--- src/App.js
+++ src/App.js
@@ -5,20 +5,12 @@ import Footer from "./components/Footer";
 import PhotoStack from "./components/PhotoStack";
 
 function App() {
-  const photos = [
-    { src: "/images/photo1.jpg", alt: "Photo 1", credit: "John Doe" },
-    { src: "/images/photo2.jpg", alt: "Photo 2", credit: "Jane Smith" },
-    { src: "/images/photo3.jpg", alt: "Photo 3", credit: "Photography Inc." },
-    { src: "/images/photo4.jpg", alt: "Photo 4", credit: "Another Photographer" },
-    { src: "/images/photo5.jpg", alt: "Photo 5", credit: "Creative Artist" },
-  ];
-
   return (
     <div className="App">
       <Header />
       <main>
         <h1>Hello Daily Bruin!</h1>
-        <PhotoStack photos={photos} />
+        <PhotoStack />
       </main>
       <Footer />
     </div>
--- src/components/PhotoStack.js
+++ src/components/PhotoStack.js
@@ -1,7 +1,21 @@
 import React, { useState } from "react";
 import "./PhotoStack.css";
 
-const PhotoStack = ({ photos }) => {
+//import local images for testing
+import photo1 from "../images/photo1.jpg";
+import photo2 from "../images/photo2.jpg";
+import photo3 from "../images/photo3.jpg";
+import photo4 from "../images/photo4.jpg";
+
+const PhotoStack = () => {
+  const photos = [
+    //can change "photo1" etc. to link to photo assets
+    { src: photo1, alt: "Photo 1", credit: "Armaan Bassi" },
+    { src: photo2, alt: "Photo 2", credit: "Jane Smith" },
+    { src: photo3, alt: "Photo 3", credit: "John Doe" },
+    { src: photo4, alt: "Photo 4", credit: "Emily Brown" },
+  ];
+
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const nextPhoto = () => {
