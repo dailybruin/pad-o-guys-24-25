3353cea4fd3ebac3d5ae8c67c107dc992eba95b2
tsakshi01 <tsakshi01@gmail.com>
2025-02-04 22:18:09 -0800

    Slide #1 Components

--- src/components/ScrollingEffect.js
+++ src/components/ScrollingEffect.js
@@ -1,4 +1,10 @@
 import React, { useRef } from "react";
+import coffee from "../images/coffee.svg";
+import pencil from "../images/pencil.svg";
+import ticket from "../images/ticket.svg";
+import pizza from "../images/pizza.svg";
+import pen from "../images/pen.svg";
+import note from "../images/note.svg";
 import "../App.css";
 
 function ScrollingEffect({ slides }) {
@@ -63,46 +69,14 @@ function ScrollingEffect({ slides }) {
               />
             </svg>
           </div>
-          {index === 0 && (
-          <div className="coffee-background">
-            <svg width="663" height="615" viewBox="0 0 663 615" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M428.26 149.95L555.952 100.314C578.704 95.0467 599.361 141.194 576.242 152.509L448.55 202.145L428.26 149.95Z" fill="#FFF3D6"/>
-            <path d="M452.091 139.642L406.526 157.314L425.845 209.9L451.347 200.212C451.347 200.212 457.606 197.746 477.025 190.056C496.445 182.366 482.509 129.656 452.091 139.642Z" fill="#FFE299"/>
-            <g filter="url(#filter0_d_1739_1360)">
-            <circle cx="278.53" cy="248.905" r="180.5" transform="rotate(65.4721 278.53 248.905)" fill="#FFF3D6"/>
-            </g>
-            <circle cx="279.192" cy="249.153" r="158" transform="rotate(65.4721 279.192 249.153)" fill="#99582A"/>
-            <g filter="url(#filter1_f_1739_1360)">
-            <path d="M296.028 267.518C321.419 75.8793 166.902 60.4262 128.182 56.7888C18.3478 46.471 -58.1086 -7.99625 -56.8199 -65.9582C-55.7889 -112.328 -98.5228 -176.778 -146.826 -201.236C-201.883 -215.2 -168.737 -283.652 -153.373 -344.234" stroke="#E9D1C3" stroke-opacity="0.5" stroke-width="40" stroke-linecap="round"/>
-            </g>
-            <g filter="url(#filter2_f_1739_1360)">
-            <path d="M245.346 309.421C216.803 253.048 168.99 130.141 96.8828 188.927C62.6216 216.858 -23.1851 270.621 -62.3746 177.008" stroke="#E9D1C3" stroke-opacity="0.5" stroke-width="20" stroke-linecap="round"/>
-            </g>
-            <path fill-rule="evenodd" clip-rule="evenodd" d="M413.028 216.471C410.834 226.513 407.709 236.5 403.607 246.322C366.894 334.234 265.866 375.738 177.955 339.025C173.297 337.08 168.77 334.954 164.377 332.66C178.821 351.543 198.258 367.01 221.77 376.829C293.373 406.732 375.66 372.927 405.562 301.324C417.216 273.418 419.194 243.889 413.028 216.471Z" fill="#70411F" fill-opacity="0.8"/>
-            <defs>
-            <filter id="filter0_d_1739_1360" x="93.9858" y="68.3613" width="369.088" height="369.088" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-            <feOffset dy="4"/>
-            <feGaussianBlur stdDeviation="2"/>
-            <feComposite in2="hardAlpha" operator="out"/>
-            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
-            <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1739_1360"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1739_1360" result="shape"/>
-            </filter>
-            <filter id="filter1_f_1739_1360" x="-267.376" y="-434.239" width="656.205" height="791.76" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-            <feGaussianBlur stdDeviation="35" result="effect1_foregroundBlur_1739_1360"/>
-            </filter>
-            <filter id="filter2_f_1739_1360" x="-132.377" y="103.38" width="447.725" height="276.043" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
-            <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/>
-            <feGaussianBlur stdDeviation="30" result="effect1_foregroundBlur_1739_1360"/>
-            </filter>
-            </defs>
-            </svg>
-          </div>)}
+          {index === 0 && <img src={coffee} alt="Coffee Background" style={{ position: "absolute", top: "-15%", right: -250, width: "45em", height: "auto", zIndex: 10 }} />}
+          {index === 0 && <img src={pencil} alt="Pencil Background" style={{ position: "absolute", top: 50, left: -200, width: "40%", height: "auto", zIndex: 10 }} />}
+          {index === 0 && <img src={ticket} alt="Ticket Background" style={{ position: "absolute", top: 500, left: -350, width: "20%", height: "auto", zIndex: 10 }} />}
+          {index === 0 && <img src={pizza} alt="Pizza Background" style={{ position: "absolute", bottom: "42%", right: "-30%", width: "50%", height: "auto", zIndex: 10 }} />}
+          {index === 0 && <img src={note} alt="Note Background" style={{ position: "absolute", top: "20%", right: "-20%", width: "30%", height: "auto", zIndex: 10 }} />}
+          {index === 0 && <img src={pen} alt="Pen Background" style={{ position: "absolute", top: "25%", right: "-20%", width: "18%", height: "auto", zIndex: 10 }} />}
+
+
 
           <div className="text-content">
             <p>{slide}</p>
