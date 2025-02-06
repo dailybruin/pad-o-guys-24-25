75474d975234e11888c4582f6bda4376e7dc5216
Henry <nothenrynguyen@gmail.com>
2025-02-05 16:35:06 -0800

    Add landing page integrated with sticky header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import DBLogo from "../images/DailyBruinLogo.svg";
 
-import DBLogo from '../images/DailyBruinLogo.svg'
+const LandingSection = styled("div")`
+  height: 100vh; /* Full viewport height */
+  background: #fffaf0; /* Match the Figma background color */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  font-family: "ITC Century";
+  font-size: 36px;
+  font-weight: bold;
+  color: black;
+`;
 
 const DBHeader = styled("div")`
   z-index: 2001;
-
   position: -webkit-sticky;
   position: sticky;
   top: 0;
@@ -12,25 +23,48 @@ const DBHeader = styled("div")`
   width: 100%;
   padding: 0.2em 0;
   color: white;
-  font-family: 'ITC Century';
+  font-family: "ITC Century";
   font-style: normal;
   font-weight: 400;
   text-align: center;
   text-transform: uppercase;
   font-size: 18px;
   line-height: 21.6px;
-  /* border-bottom: 2px solid black; */
+`;
+
+const ContentSection = styled("div")`
+  padding: 2em;
+  background: #fffaf0; /* Match the rest of the page */
+  font-family: "ITC Century";
+  color: black;
+  line-height: 1.6;
 `;
 
 const Header = () => {
   return (
-    <DBHeader>
-      {/* Daily Bruin */}
-      <a href="https://dailybruin.com">
-        <img src={DBLogo} alt="Daily Bruin" />
-      </a>
-    </DBHeader>
-  )
-}
+    <>
+      {/* Sticky Header */}
+      <DBHeader>
+        <a href="https://dailybruin.com">
+          <img src={DBLogo} alt="Daily Bruin" />
+        </a>
+      </DBHeader>
+
+      {/* Landing Section */}
+      <LandingSection>
+        <h1>illo landing</h1>
+      </LandingSection>
+
+      {/* Page Contents */}
+      <ContentSection>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
+          facilisi. Donec id justo. Praesent porttitor...
+        </p>
+        {/* Add additional content here */}
+      </ContentSection>
+    </>
+  );
+};
 
 export default Header;
